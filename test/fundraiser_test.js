362199309ea1135c52a847d6d58071ae522007d0
ryoukata <ryoya3091@gmail.com>
2021-08-20 14:31:24 +0900

    feat(#3): add test and function to change beneficiary by owner.

--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -6,17 +6,21 @@ contract("Fundraiser", accounts => {
   const url = "beneficiaryname.org";
   const imageURL = "https://placekitten.com/600/350";
   const description = "Beneficiary description";
+  const beneficiary = accounts[1];
+  const owner = accounts[0];
+
+  beforeEach (async () => {
+    fundraiser = await FundraiserContract.new(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary,
+      owner
+    )
+  });
 
   describe("initialization", () => {
-    beforeEach (async () => {
-      fundraiser = await FundraiserContract.new(
-        name,
-        url,
-        imageURL,
-        description
-      )
-    });
-
     it("gets the beneficiary name", async () => {
       const actual = await fundraiser.name();
       assert.equal(actual, name, "name should match");
@@ -35,6 +39,37 @@ contract("Fundraiser", accounts => {
     it("gets the beneficiary description", async () => {
       const actual = await fundraiser.description();
       assert.equal(actual, description, "description should match");
+    });
+
+    it("gets the beneficiary", async () => {
+      const actual = await fundraiser.beneficiary();
+      assert.equal(actual, beneficiary, "beneficiary addresses should match");
+    });
+
+    it("gets the owner", async () => {
+      const actual = await fundraiser.owner();
+      assert.equal(actual, owner, "bios should match");
+    });
+  });
+
+  describe("setBeneficiary", () => {
+    const newBeneficiary = accounts[2];
+
+    it("updated beneficiary when called by owner account", async () => {
+      await fundraiser.setBeneficiary(newBeneficiary, {from: owner});
+      const actualBeneficiary = await fundraiser.beneficiary();
+      assert.equal(actualBeneficiary, newBeneficiary, "beneficiaries should match");
+    });
+
+    it("throws an error when called from a non-owner account", async () => {
+      try {
+        await fundraiser.setBeneficiary(newBeneficiary, {from: accounts[3]});
+        assert.fail("withdraw was not restricted to owners");
+      } catch(err) {
+        const expectedError = "Ownable: caller is not the owner";
+        const actualError = err.reason;
+        assert.equal(actualError, expectedError, "should not be permitted");
+      }
     });
   });
 });
