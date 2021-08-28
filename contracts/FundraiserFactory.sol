f9db898e8aa139d7bc8cdf275101e0c720e953eb
ryoukata <ryoya3091@gmail.com>
2021-08-28 17:20:52 +0900

    feat(#11): display fundraiser instance

--- contracts/FundraiserFactory.sol
+++ contracts/FundraiserFactory.sol
@@ -3,6 +3,9 @@ pragma solidity >= 0.8.0;
 import "./Fundraiser.sol";
 
 contract FundraiserFactory {
+  // max item return counts from fundraisers function
+  uint256 constant maxLimit = 20;
+
   Fundraiser[] private _fundraisers;
 
   event FundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
@@ -30,5 +33,20 @@ contract FundraiserFactory {
     );
     _fundraisers.push(fundraiser);
     emit FundraiserCreated(fundraiser, fundraiser.owner());
+  }
+
+  function fundraisers(uint256 limit, uint256 offset) public view returns(Fundraiser[] memory coll) {
+    require(offset <= fundraisersCount(), "offset out of bounds");
+
+    uint256 size = fundraisersCount() - offset;
+    size = size < limit ? size : limit;
+    size = size < maxLimit ? size : maxLimit;
+    coll = new Fundraiser[](size);
+
+    for(uint256 i = 0; i < size; i++) {
+      coll[i] = _fundraisers[offset + i];
+    }
+
+    return coll;
   }
 }
